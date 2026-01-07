888634e7a74708f1c11c2d44aeb170448f8db1e7
jesse1231234 <jesse.thompson@colostate.edu>
2026-01-07 08:33:22 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -214,8 +214,9 @@ async def analyze(
             kpis=kpis,
             echo_module_df=echo_module_sorted,
             gradebook_module_df=gb_module_sorted,
-            gradebook_summary_df=gradebook_summary_df,
+            gradebook_summary_df=gradebook_tables.gradebook_summary_df,
         )
+
     except Exception as e:
         analysis_error = str(e)
 
